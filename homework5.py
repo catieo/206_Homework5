65637be3edbed2770b17a202a171e4f859ba6b65
Catie <catieo@umich.edu>
2017-10-09 19:42:19 -0400

    Completed homework 5

--- homework5.py
+++ homework5.py
@@ -1,1 +1,16 @@
 import re 
+
+filevar = open("regex_sum_39677.txt")
+
+list_of_nums = [] 
+for line in filevar.readlines():
+	line = line.rstrip()
+	num = re.findall('[0-9]+', line)
+	if num != []:
+		list_of_nums.extend(num)
+
+result = 0
+for x in list_of_nums:
+	result = result + int(x)
+
+print(result)
